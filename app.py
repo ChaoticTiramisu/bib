37bad5a374d28caa7c151f2661ba2f6e2b6aae20
ChaoticTiramisu <cairobe2007@gmail.com>
2024-03-29 11:44:30 +0100

    mega push

--- app.py
+++ app.py
@@ -1,46 +1,73 @@
-from flask import Flask, render_template, redirect,url_for,request,flash,session 
+from flask import Flask, render_template, redirect, url_for, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
 import os
-from database import Bibliothecaris,Ontlener,Boeken
+from database import Bibliothecaris, Ontlener, Boeken, Login
 
 dirname = os.path.dirname(__file__)
 app = Flask(__name__, instance_path=dirname)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "sqlalchemy"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/sportweb.db"
-app.secret_key = "Arno_augu"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/bib.db"
+app.secret_key = "Arno_augu_Cairo"
 db = SQLAlchemy(app)
 with app.app_context():
     db.create_all()
 
 
-
-app = Flask(__name__)
-
 @app.route("/")
 def index():
+    name = session.get('email')
+    if name is not None:
+        return redirect(url_for("login"))
     return render_template("index.html")
 
-@app.route("/boeken")
-def boeken():
-    return render_template("boeken.html")
 
-@app.route("/button")
-def button():
-    return render_template("button_test.html")
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        email = request.form["login_email"]
+        password = request.form["login_paswoord"]
+        user = db.session.query(Login).filter_by(user_email=email).first()
+        if user is not None and user.user_paswoord == password:
+            session["email"] = email
+            flash("Login succesvol")
+            return redirect("/")
+        else:
+            flash("Paswoord incorrect of de gebruiker bestaat nog niet.")
+            return redirect(url_for("login"))
+    return render_template("login.html")
 
-@app.route("/button/post", methods = ['POST'])
-def post():
-    bibliothecaris(request.form['bib_id'],request.form['voornaam'],request.form['achternaam'],request.form['email'],request.form['bib_recht'],request.form['tel_nr_bib'])
-    flash("Je bent geregistreerd!")
-    return(url_for(index))
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["register_email"]
+        password = request.form["register_paswoord"]
+
+       
+        new_ontlener = Ontlener(lln_naam=name)
+        db.session.add(new_ontlener)
+        db.session.commit()
+
+        
+        new_login = Login(lln_id=new_ontlener.lln_id, user_email=email, user_paswoord=password)
+        db.session.add(new_login)
+        db.session.commit()
+
+        session["email"] = email
+        flash("Registratie succesvol")
+        return redirect(url_for("login"))
+
+    return render_template("register.html")
 
 
-
-
+@app.route("/logout")
+def logout():
+    session["email"] = None
+    return redirect("/")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
--- database.py
+++ database.py
@@ -1,53 +1,58 @@
-from sqlalchemy import String,Integer,Boolean,ForeignKey,create_engine
-from sqlalchemy.orm import DeclarativeBase,Mapped,sessionmaker,mapped_column,relationship
+from sqlalchemy import String, Integer, ForeignKey, create_engine
+from sqlalchemy.orm import relationship, declarative_base, mapped_column
+from sqlalchemy_utils import database_exists, create_database
 
+engine = create_engine("sqlite:///instance/bib.db", echo=True)
 
-engine = create_engine("sqlite:///databases/Bib.db", echo=True)
+if not database_exists("sqlite:///instance/bib.db"):
+    create_database(engine.url)
 
-class Base(DeclarativeBase):
-    pass
-
-class Bibliothecaris(Base):
-    __tablename__ = "Bibliothecaris"
-
-    biblio_bib_id:Mapped[int] = mapped_column(primary_key = True)
-    biblio_naam:Mapped[str] = mapped_column()
-    biblio_achternaam:Mapped[str] = mapped_column()
-    biblio_email:Mapped[str] = mapped_column()
-    biblio_bib_recht:Mapped[str] = mapped_column()
-    biblio_tel_nr:Mapped[int] = mapped_column()
+Base = declarative_base()
 
 class Ontlener(Base):
     __tablename__ = "Ontlener"
 
-    lln_id:Mapped[int] = mapped_column(primary_key=True)
-    lln_naam:Mapped[str] = mapped_column()
-    lln_achternaam:Mapped[str] = mapped_column()
-    lln_email:Mapped[str] = mapped_column()
-    lln_geboortedtm:Mapped[str] = mapped_column()
-    lln_tel_nr:Mapped[int] = mapped_column()
+    lln_id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    lln_naam = mapped_column(String)
+    lln_achternaam = mapped_column(String)
+    lln_geboortedtm = mapped_column(String)
+    lln_tel_nr = mapped_column(Integer)
+   
+    login_id = mapped_column(Integer, ForeignKey('Login.lln_id'), unique=True)
+    login = relationship("Login", back_populates="ontlener", foreign_keys=[login_id])
+
+
+class Bibliothecaris(Base):
+    __tablename__ = "Bibliothecaris"
+
+    biblio_bib_id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    biblio_naam = mapped_column(String)
+    biblio_achternaam = mapped_column(String)
+    biblio_email = mapped_column(String)
+    biblio_bib_recht = mapped_column(String)
+    biblio_tel_nr = mapped_column(Integer)
+    
+    login_id = mapped_column(Integer, ForeignKey('Login.biblio_bib_id'), unique=True)
+    login = relationship("Login", back_populates="bibliothecaris", foreign_keys=[login_id])
 
 class Boeken(Base):
-    __tablename__="Boeken"
+    __tablename__ = "Boeken"
 
-    bkn_genre:Mapped[str] = mapped_column()
-    bkn_thema:Mapped[str] = mapped_column()
-    bkn_auteur:Mapped[str] = mapped_column()
+    bkn_ISBN = mapped_column(String, primary_key=True)
+    bkn_genre = mapped_column(String)
+    bkn_thema = mapped_column(String)
+    bkn_auteur = mapped_column(String)
+
+class Login(Base):
+    __tablename__ = "Login"
     
+    login_id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    lln_id = mapped_column(Integer, ForeignKey("Ontlener.lln_id"), remote_side=[Ontlener.lln_id])
+    biblio_bib_id = mapped_column(Integer, ForeignKey("Bibliothecaris.biblio_bib_id"), remote_side=[Bibliothecaris.biblio_bib_id])
+    user_email = mapped_column(String)
+    user_paswoord = mapped_column(String)
 
+    ontlener = relationship("Ontlener", back_populates="login", foreign_keys=[lln_id])
+    bibliothecaris = relationship("Bibliothecaris", back_populates="login", foreign_keys=[biblio_bib_id])
 
 Base.metadata.create_all(bind=engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-def bibliothecaris(Bib_id,Naam,Achternaam,Email,Bib_recht,Tel_nr_bib):
-
-    persoon = Bibliothecaris(biblio_bib_id = Bib_id,
-                             biblio_naam = Naam,
-                             biblio_achternaam = Achternaam,
-                             biblio_email = Email,
-                             biblio_bib_recht =Bib_recht,
-                             biblio_tel_nr = Tel_nr_bib)
-    session.add(persoon)
-    session.commit()
